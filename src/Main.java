24a86b0c0a59212efbdbc5579a367c2b4d6e9725
AynurZaydullin <zaydullin.aynur@yandex.ru>
2022-11-25 17:44:59 +0300

    Сделано ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        //Раздел "Циклы". Часть 1.
+        System.out.println("Задание 1.");
+        System.out.println();
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Задание 2.");
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Задание 3.");
+        for (int i = 0; i <= (17-1); i = i + 2){
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Задание 4.");
+        for (int i = 10; i >= -10; i--){
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println();
 
     }
 }
